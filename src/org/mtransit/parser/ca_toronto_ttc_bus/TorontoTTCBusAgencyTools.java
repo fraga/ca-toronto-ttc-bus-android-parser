ad054c31aef08645c2fca9ee167c66a8dd0863f7
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2015-03-09 00:50:03 -0400

    remove args comments

--- src/org/mtransit/parser/ca_toronto_ttc_bus/TorontoTTCBusAgencyTools.java
+++ src/org/mtransit/parser/ca_toronto_ttc_bus/TorontoTTCBusAgencyTools.java
@@ -4,9 +4,6 @@ import org.mtransit.parser.DefaultAgencyTools;
 
 public class TorontoTTCBusAgencyTools extends DefaultAgencyTools{
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
