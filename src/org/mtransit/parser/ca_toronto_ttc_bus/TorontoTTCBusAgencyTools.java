044322f10f79876ec58e165308990fd830db0e83
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2015-03-09 07:42:42 -0400

    Override start, pretty print status

--- src/org/mtransit/parser/ca_toronto_ttc_bus/TorontoTTCBusAgencyTools.java
+++ src/org/mtransit/parser/ca_toronto_ttc_bus/TorontoTTCBusAgencyTools.java
@@ -1,8 +1,18 @@
 package org.mtransit.parser.ca_toronto_ttc_bus;
 
 import org.mtransit.parser.DefaultAgencyTools;
+import org.mtransit.parser.Utils;
 
 public class TorontoTTCBusAgencyTools extends DefaultAgencyTools {
+
+    @Override
+    public void start(String[] args) {
+        System.out.printf("Generating TTC bus data...\n");
+        long start = System.currentTimeMillis();
+        super.start(args);
+        System.out.printf("Generating TTC bus data... DONE in %s.\n",
+                Utils.getPrettyDuration(System.currentTimeMillis() - start));
+    }
 
     public static void main(String[] args) {
         TorontoTTCBusAgencyTools ttcTools = new TorontoTTCBusAgencyTools();
